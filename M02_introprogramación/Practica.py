abc21e2dd66aafdfff8a2519264b96a4b84b9ee3
Hider Geovanny Rosero Erazo <hroseroe@sena.edu.co>
2023-09-11 12:15:29 -0500

    actualizacion

--- M02_introprogramación/Practica.py
+++ M02_introprogramación/Practica.py
@@ -3,8 +3,19 @@
 
 # A partir del próximo módulo: M03_variablesydatos vas a tener actividades para realizar.
 
-print('Hola Mundo!')
-a = 20
-while a > 0:
-    print(a)
-    a -= 1
+# print('Hola Mundo!')
+# a = 20
+# while a > 0:
+#     print(a)
+#     a -= 1
+
+
+num_1 = 3    # Preguntamos por un primer número.
+num_2 = 4  # Luego preguntamos por un segundo número.
+
+if num_1 > num_2:       # Si el primer número es mayor que el segundo.
+    print('El primer número es mayor que el segundo.')  # Imprimimos esta expresión.
+elif num_1 < num_2:     # En caso de que el segundo sea mayor.
+    print('El segundo número es mayor que el primero.') # Imprimiremos esta expresión.
+else:   # En caso de que no cumpla ninguna condición.
+    print('Los 2 números son iguales.')
